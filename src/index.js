437369d760b848405b0f0d04afeee6199a8b06ec
Andrew Hawileh <andrewh829@gmail.com>
2023-03-01 10:52:42 -0600

    done

--- src/index.js
+++ src/index.js
@@ -1,1 +1,73 @@
 // write your code here
+//http://localhost:3000/ramens
+document.addEventListener('DOMContentLoaded', run)
+
+
+function run()
+{
+    fetch('http://localhost:3000/ramens')
+    .then(resp => resp.json())
+    .then(ramens => ramens.forEach(el => showRamen(el)))
+    const form = document.getElementById('new-ramen')
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
+        const newName = document.getElementById('new-name').value
+        const newRest = document.getElementById('new-restaurant').value
+        const newImg = document.getElementById('new-image').value
+        const newRating = document.getElementById('new-rating').value
+        const newCommment = document.getElementById('new-comment').value
+        const newRamen = {
+            name: newName,
+            restaurant: newRest,
+            image: newImg,
+            rating: newRating,
+            comment:newCommment 
+        }
+        fetch('http://localhost:3000/ramens', {
+            method: 'POST',
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify(newRamen)
+        })
+        .then(resp => resp.json())
+        .then(a => showRamen(a))
+        e.target.reset();
+    })
+}
+//Click on an image from the #ramen-menu div and see all the info about that ramen displayed inside the 
+//#ramen-detail div and where it says insert comment here and insert rating here.
+function showRamen(ramen)
+{
+    const imgCont = document.getElementById('ramen-menu')
+    const rImg = document.createElement('img')
+    const del = document.createElement('button')
+    rImg.src = ramen.image
+    rImg.dataset.id = ramen.id;
+    rImg.addEventListener('click', handleClick)
+    imgCont.append(rImg)
+}
+
+function handleClick(e)
+{
+    //e.target.dataset.id
+    //img goes in img.detail-image
+    //name goes in h2.name
+    //rest goes in h3.restaurant
+    //rating goes in #rating-display
+    //comment goes in #comment-display
+    const img = document.querySelector('img.detail-image')
+    const name = document.querySelector('h2.name')
+    const rest = document.querySelector('h3.restaurant')
+    const rating = document.getElementById('rating-display')
+    const comment = document.getElementById('comment-display')
+    fetch(`http://localhost:3000/ramens/${e.target.dataset.id}`)
+    .then(resp => resp.json())
+    .then((ramen) => {
+        img.src = ramen.image;
+        name.textContent = ramen.name
+        rest.textContent = ramen.restaurant
+        rating.textContent = ramen.rating
+        comment.textContent = ramen.comment
+    })
+}
+
+//completed at 10:45
